d9960ff85cb472819a2f3e367e327294840eb300
Aidar <audars@mail.ru>
2023-02-08 10:03:39 +0600

    tsis3

--- tsis1/x.py
+++ tsis1/x.py
@@ -4,10 +4,11 @@ print(txt.format(price))
 
 txt = "The price is {:.2f} dollars"
 
-print(txt.format(price, itemno, count))
+# print(txt.format(price, itemno, count))
 
 quantity = 3
 itemno = 567
 price = 49
 myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+
 print(myorder.format(quantity, itemno, price))
